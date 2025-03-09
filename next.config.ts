d73187050b0f368784585ec126ff373892de4476
Syed Ali Asar <sa.asar14@gmail.com>
2025-03-09 18:21:36 -0400

    build output

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['files.stripe.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
